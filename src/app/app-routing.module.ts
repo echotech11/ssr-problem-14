8cab8223bfcf29606c0b60c90996e44482d8ad9c
Scott Ferguson <scott@echotc.com>
2023-06-02 10:58:55 -0400

    ssr is working correctly.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: LayoutComponent,
+    loadChildren: () => import('./test/test.module').then(m => m.TestModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TestService } from '../test.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent {
+
+  data = this.service.getData();
+
+  constructor(private service: TestService) { }
+}
